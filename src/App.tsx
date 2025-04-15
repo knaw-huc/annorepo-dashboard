754e5aa472f25048a9dc5e5f8a78acaef5c84b88
BasLee <basleenknegt@gmail.com>
2025-04-15 15:08:38 +0200

    Add minimal layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,27 @@
-import {useState} from 'react'
 import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
-
+export function App() {
   return (
-    <>
-      <h1 className="text-3xl font-bold underline">annorepo-dashboard</h1>
-      {count} <button type="button" onClick={() => setCount(count + 1)}>+</button>
-    </>
+    <div className="flex flex-col min-h-screen">
+      <header className="p-4 border-b">
+        <p>annorepo-dashboard</p>
+      </header>
+      <div className="flex flex-col md:flex-row flex-grow">
+        <aside
+          className="w-full md:w-64 p-4 order-1 md:order-1 border-r"
+        >
+          <ul>
+            <li>Home</li>
+            <li>Containers</li>
+          </ul>
+        </aside>
+        <main className="flex-grow p-4 order-2 md:order-2">
+          <p>Content</p>
+        </main>
+      </div>
+      <footer className="p-4 border-t">
+        <p>Footer</p>
+      </footer>
+    </div>
   )
 }
-
-export default App
