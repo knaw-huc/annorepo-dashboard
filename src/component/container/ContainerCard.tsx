012340dcd59fc1b5eec1c9b3d3261b773e4da82f
BasLee <basleenknegt@gmail.com>
2025-04-24 14:46:08 +0200

    Link from container card to container page

--- src/component/container/ContainerCard.tsx
+++ src/component/container/ContainerCard.tsx
@@ -5,6 +5,7 @@ import {Card} from "../common/Card.tsx";
 import {A} from "../common/A.tsx";
 import {External} from "../common/icon/External.tsx";
 import {Pipe} from "../common/Pipe.tsx";
+import {Link} from "@tanstack/react-router";
 
 export function ContainerCard(props: PropsWithChildren<{
   container: ArContainer
@@ -16,9 +17,14 @@ export function ContainerCard(props: PropsWithChildren<{
   }
 
   return <Card
-    header={<h5 className="mt-2 text-xl font-medium leading-tight text-center">
+    header={<Link
+      to="/container/$containerId"
+      params={{containerId: container.id}}
+    ><h5
+      className="mt-2 text-xl font-medium leading-tight text-center"
+    >
       {container.label || ''}
-    </h5>}
+    </h5></Link>}
     footer={<A href={container.first.id}>
       Browse annotations
       <External/>
@@ -26,12 +32,12 @@ export function ContainerCard(props: PropsWithChildren<{
   >
     <div>
       <p className="mb-2">
+        {container.total} annotations
+        <Pipe/>
         <A href={container.id}>
           Source
           <External/>
         </A>
-        <Pipe/>
-        {container.total} annotations
       </p>
       <p>
         Types: {container.type.join(', ')}
