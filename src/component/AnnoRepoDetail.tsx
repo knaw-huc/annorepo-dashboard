21e61c20478cc2a1ea0e2a28ccd897aeebf95e26
Bas Leenknegt <basleenknegt@gmail.com>
2025-05-13 14:01:28 +0200

    Add some whitespace to detail card

--- src/component/AnnoRepoDetail.tsx
+++ src/component/AnnoRepoDetail.tsx
@@ -14,18 +14,27 @@ export function AnnoRepoDetail() {
         <H1>{about.appName}</H1>
         <div className="grid grid-cols-5 gap-5">
           <Card
-            header={<p className="text-sm text-slate-500">AnnoRepo Version</p>}>
-            <p
-              className="font-bold">{about.version}</p></Card>
-          <Card
-            header={<p className="text-sm text-slate-500">Running since</p>}><p
-            className="font-bold">{new Date(about.startedAt).toLocaleString()}</p>
+            header={<p className="text-sm text-slate-500">AnnoRepo Version</p>}
+          >
+            <p className="font-bold">
+              {about.version}
+            </p>
           </Card>
-          <Card header={<p className="text-sm text-slate-500">Running at</p>}><A
-            href={about.baseURI} className="font-bold">
-            {about.baseURI}
-            <External className="ml-1"/>
-          </A></Card>
+          <Card
+            header={<p className="text-sm text-slate-500">Running since</p>}
+          >
+            <p className="font-bold">
+              {new Date(about.startedAt).toLocaleString()}
+            </p>
+          </Card>
+          <Card
+            header={<p className="text-sm text-slate-500">Running at</p>}
+          >
+            <A href={about.baseURI} className="font-bold">
+              {about.baseURI}
+              <External className="ml-1"/>
+            </A>
+          </Card>
         </div>
       </>
       : <Loading/>
