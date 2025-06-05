6b037eabec61e988540ff0646ae70c17a40a8702
Bas Leenknegt <basleenknegt@gmail.com>
2025-06-05 16:38:55 +0200

    Remove TODOs

--- src/component/custom-query/GlobalQueryEditor.tsx
+++ src/component/custom-query/GlobalQueryEditor.tsx
@@ -19,13 +19,6 @@ export function GlobalQueryEditor(props: {
   const {query, onChange} = props;
   const [pageNo, setPageNo] = useState(0);
 
-  /**
-   * TODO:
-   *  - global values <--> custom query params
-   *  - create custom query
-   *  - show results of existing custom query
-   *  - edit query of custom query, to create new custom query
-   */
   const myContainers = useGet('/my/containers') as QR<ArMyContainers>
   const containerNames = getContainerNames(myContainers.data)
   const fields = useContainerFields(containerNames[0] ?? '')
