3608abc0514272402ef0b18bac18a019dab68e2b
Bas Leenknegt <basleenknegt@gmail.com>
2025-08-14 13:28:32 +0200

    Fix comments on route generation

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,6 @@ import ReactDOM from "react-dom/client";
 import { createRouter, RouterProvider } from "@tanstack/react-router";
 import "./main.css";
 
-// generated route tree
 import { routeTree } from "./routeTree.gen";
 import { QueryClientProvider } from "@tanstack/react-query";
 import { OpenApiClientProvider } from "./client/OpenApiClientProvider.tsx";
@@ -11,10 +10,12 @@ import { createQueryClient } from "./client/QueryClient.tsx";
 import { ConfigProvider } from "./component/ConfigProvider.tsx";
 import { orThrow } from "./util/orThrow.ts";
 import { Config } from "./component/Config.ts";
+import { StrictMode } from "react";
 
+// Generate routes from ./src/routes/
 const router = createRouter({ routeTree });
 
-// Register the router instance for type safety
+// Register generated route types
 declare module "@tanstack/react-router" {
   interface Register {
     router: typeof router;
