4050da22c4765b571e5eb2e4945ac7e989af1e88
Bas Leenknegt <basleenknegt@gmail.com>
2025-08-20 15:34:35 +0200

    No empty warning div when warning is falsy

--- src/component/common/Warning.tsx
+++ src/component/common/Warning.tsx
@@ -1,17 +1,21 @@
-import {PropsWithChildren} from "react";
+import { PropsWithChildren } from "react";
+import { isEmpty } from "lodash";
 
-export function Warning(props: PropsWithChildren<{
-  className?: string,
-
-}>) {
-  let className = 'px-5 py-2 text-sm text-red-800 rounded-lg bg-red-50'
+export function Warning(
+  props: PropsWithChildren<{
+    className?: string;
+  }>,
+) {
+  let className = "px-5 py-2 text-sm text-red-800 rounded-lg bg-red-50";
   if (props.className) {
-    className += ` ${props.className}`
+    className += ` ${props.className}`;
   }
-  return <div
-    className={className}
-    role="alert"
-  >
-    {props.children}
-  </div>
+  if (isEmpty(props.children)) {
+    return null;
+  }
+  return (
+    <div className={className} role="alert">
+      {props.children}
+    </div>
+  );
 }
