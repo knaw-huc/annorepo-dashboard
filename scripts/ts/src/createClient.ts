b6a3a551c389fd3b03a01dc7093ccc7815b309ca
Bas Leenknegt <basleenknegt@gmail.com>
2025-08-27 10:43:55 +0200

    Throw error when no filename found in OPEN_API_ENDPOINT

--- scripts/ts/src/createClient.ts
+++ scripts/ts/src/createClient.ts
@@ -1,24 +1,26 @@
-import openapiTS, {astToString} from "openapi-typescript";
-import * as fs from 'fs';
+import openapiTS, { astToString } from "openapi-typescript";
+import * as fs from "fs";
 
-const openApiEndpoint = process.env.OPEN_API_ENDPOINT
-  || 'http://localhost:8080/openapi.json'
-const schemaFilename = process.env.RESULT_TYPES_PATH
-  || "../../src/openapi.ts";
+const openApiEndpoint =
+  process.env.OPEN_API_ENDPOINT || "http://localhost:8080/openapi.json";
+const schemaFilename = process.env.RESULT_TYPES_PATH || "../../src/openapi.ts";
 
-main()
+main();
 
 async function main() {
   const openApiUrl = new URL(openApiEndpoint);
-  const openApiFilename = openApiUrl.pathname.split('/').pop()
+  const openApiFilename = openApiUrl.pathname.split("/").pop();
+  if (!openApiFilename) {
+    throw new Error(`Could not extract filename from ${openApiEndpoint}`);
+  }
   const openApiJson = await fetch(openApiUrl)
-    .catch(e => {
-      const cause = e instanceof Error ? e.message : 'unknown';
+    .catch((e) => {
+      const cause = e instanceof Error ? e.message : "unknown";
       throw new Error(`Could not fetch ${openApiEndpoint}, cause: ${cause}`);
     })
-    .then(r => r.json())
+    .then((r) => r.json());
 
-  const ast = await openapiTS(openApiJson)
+  const ast = await openapiTS(openApiJson);
   const contents = astToString(ast);
   fs.writeFileSync(openApiFilename!, JSON.stringify(openApiJson));
   fs.writeFileSync(schemaFilename, contents);
