7a570089509d2f5f8ec6cc6b3c43dff79823fe5d
Bas Leenknegt <basleenknegt@gmail.com>
2025-09-25 13:26:41 +0200

    Add button to home when logged out

--- src/component/auth/LoginMessageCard.tsx
+++ src/component/auth/LoginMessageCard.tsx
@@ -1,4 +1,5 @@
 import { LoginButton } from "./LoginButton.tsx";
+import { Button } from "../common/Button.tsx";
 
 export function LoginMessageCard(props: { message: string }) {
   return (
@@ -6,7 +7,10 @@ export function LoginMessageCard(props: { message: string }) {
       <div className="p-15 rounded shadow text-center">
         <p className="mt-5 font-medium">{props.message}</p>
         <p className="mt-10">
-          <LoginButton />
+          <Button onClick={() => (window.location.href = `/`)}>Home</Button>
+          <span className="ml-3">
+            <LoginButton />
+          </span>
         </p>
       </div>
     </div>
