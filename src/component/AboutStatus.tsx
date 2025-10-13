2ea08467be71c0afb9ce9c1c63f7d967605b7691
Bas Leenknegt <basleenknegt@gmail.com>
2025-10-13 16:52:48 +0200

    Make whole custom query card clickable

--- src/component/AboutStatus.tsx
+++ src/component/AboutStatus.tsx
@@ -1,10 +1,10 @@
 import { useAbout } from "../client/endpoint/useAbout.tsx";
-import { Loading } from "./common/Loading.tsx";
+import { StatusMessage } from "./common/StatusMessage.tsx";
 
 export function AboutStatus() {
   const about = useAbout();
   if (!about.data) {
-    return <Loading name="about" />;
+    return <StatusMessage name="about" requests={[about]} />;
   }
   return (
     <div className="hidden xl:flex flex-col gap-4 text-xs text-stone-500 border-t border-stone-200 pt-8">
--- src/component/custom-query/CustomQueryCard.tsx
+++ src/component/custom-query/CustomQueryCard.tsx
@@ -1,6 +1,6 @@
 import { PropsWithChildren } from "react";
 import { Card } from "../common/Card.tsx";
-import { Link } from "@tanstack/react-router";
+import { useNavigate } from "@tanstack/react-router";
 import { H5 } from "../common/H5.tsx";
 import { useGet } from "../../client/query/useGet.tsx";
 import { StatusMessage } from "../common/StatusMessage.tsx";
@@ -13,6 +13,7 @@ export function CustomQueryCard(
     onClick: () => void;
   }>,
 ) {
+  const navigate = useNavigate();
   const { customQueryName } = props;
   const customQuery = useGet("/global/custom-query/{customQueryName}", {
     params: { path: { customQueryName } },
@@ -24,40 +25,43 @@ export function CustomQueryCard(
 
   const createdDateTime = new Date(customQuery.data.created).toLocaleString();
   return (
-    <Card
-      className="bg-anrep-pink-50 text-anrep-pink-800 max-w-4xl cursor-pointer"
-      header={
-        <div className="flex justify-between border-b">
-          <Link
-            className="no-underline"
-            to="/custom-query/$customQueryName"
-            params={{ customQueryName }}
-          >
-            <H5>{customQueryName}</H5>
-          </Link>
-          <div className="p-4">
-            <img src="/images/icon-query.png" className="h-4 w-4" alt="" />
-          </div>
-        </div>
+    <span
+      onClick={() =>
+        navigate({
+          to: "/custom-query/$customQueryName",
+          params: { customQueryName },
+        })
       }
     >
-      <div className="flex gap-4">
-        <div className="flex-1">
-          <p className="">{customQuery.data.description}</p>
+      <Card
+        className="bg-anrep-pink-50 text-anrep-pink-800 max-w-4xl cursor-pointer"
+        header={
+          <div className="flex justify-between border-b">
+            <H5>{customQueryName}</H5>
+            <div className="p-4">
+              <img src="/images/icon-query.png" className="h-4 w-4" alt="" />
+            </div>
+          </div>
+        }
+      >
+        <div className="flex gap-4">
+          <div className="flex-1">
+            <p className="">{customQuery.data.description}</p>
 
-          <p className="pt-2">
-            {customQuery.data.createdBy
-              ? `Created by ${customQuery.data.createdBy} at ${createdDateTime}`
-              : `Created at ${createdDateTime}`}
-          </p>
+            <p className="pt-2">
+              {customQuery.data.createdBy
+                ? `Created by ${customQuery.data.createdBy} at ${createdDateTime}`
+                : `Created at ${createdDateTime}`}
+            </p>
+          </div>
+          <div className="flex-shrink-0">
+            <p className="px-4 flex flex-col">
+              <strong className="">Label</strong>
+              {customQuery.data.label}
+            </p>
+          </div>
         </div>
-        <div className="flex-shrink-0">
-          <p className="px-4 flex flex-col">
-            <strong className="">Label</strong>
-            {customQuery.data.label}
-          </p>
-        </div>
-      </div>
-    </Card>
+      </Card>
+    </span>
   );
 }
