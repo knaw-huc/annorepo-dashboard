4605485e8c9845821d83b4d87add06c54c0c4b81
Bas Leenknegt <basleenknegt@gmail.com>
2025-10-22 14:24:36 +0200

    Fix z-index of modal (vs z-index of query form)

--- src/component/Modal.tsx
+++ src/component/Modal.tsx
@@ -22,7 +22,7 @@ export function Modal(
 
   return (
     <div
-      className="fixed inset-0 flex items-center justify-center"
+      className="z-30 fixed inset-0 flex items-center justify-center"
       style={{ background: "rgba(0, 0, 0, 0.5)" }}
     >
       <div className="relative bg-white rounded-lg p-6 max-w mx-4">
