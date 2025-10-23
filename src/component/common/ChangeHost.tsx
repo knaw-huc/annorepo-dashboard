f45a4d3bb2aacb893740d98665dfc54ad6a18b2d
Bas Leenknegt <basleenknegt@gmail.com>
2025-10-23 10:46:26 +0200

    Hide change annorepo host btn when only one host

--- src/component/common/ChangeHost.tsx
+++ src/component/common/ChangeHost.tsx
@@ -1,10 +1,16 @@
 import { SelectHostModal } from "../host/SelectHostModal.tsx";
 import { useState } from "react";
 import { NeutralButton } from "./NeutralButton.tsx";
+import { useConfig } from "../ConfigProvider.tsx";
+import { keys } from "lodash";
 
 export function ChangeHost() {
   const [isRepoModalOpen, setIsRepoModalOpen] = useState(false);
+  const { AR_HOSTS } = useConfig();
 
+  if (keys(AR_HOSTS).length === 1) {
+    return null;
+  }
   return (
     <>
       <NeutralButton onClick={() => setIsRepoModalOpen(true)} className="mt-4">
